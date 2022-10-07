ec26912f71bd161e34f1ba4f4b1b55c49be51a7e
Julian Meilerman <julian.mn_95@hotmail.com>
2022-10-07 16:31:53 -0300

    Add the html and javascript of a Rock paper and scissors game

---
+++ index.js
@@ -0,0 +1,44 @@
+const choices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(){
+    let computerChoice = choices[Math.floor(Math.random()*3)];
+    return computerChoice
+}
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "Rock"){
+        if(computerSelection === "Rock"){
+            return "It's a DRAW"
+        } else if (computerSelection === "Paper"){
+            return "You LOSE! Paper beats Rock"
+        } else if (computerSelection === "Scissors"){
+            return "You WIN! Rock beats Scissors"
+        }
+    }
+
+    if (playerSelection === "Paper"){
+        if(computerSelection === "Rock"){
+            return "You WIN! Paper beats Rock"
+        } else if (computerSelection === "Paper"){
+            return "It's a DRAW"
+        } else if (computerSelection === "Scissors"){
+            return "You LOSE! Scissors beat Paper"
+        }
+    }
+
+    if (playerSelection === "Scissors"){
+        if(computerSelection === "Rock"){
+            return "You LOSE! Rock beats Scissors"
+        } else if (computerSelection === "Paper"){
+            return "You WIN! Scissors beat Paper"
+        } else if (computerSelection === "Scissors"){
+            return "It's a DRAW"
+        }
+    }
+}
+
+playerSelection = "Paper";
+computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
